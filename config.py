e39627e4d978eac37c584d3df66911f9889e9f0b
Adam Danial <adamdanial090@gmail.com>
2025-06-25 17:27:08 +0800

    Remove config.py from version control; now using .env for secrets (which I already did)

--- config.py
+++
@@ -1,8 +0,0 @@
-class Config:
-    SECRET_KEY = 'chaosweaver24'
-
-    # MySQL config (optional if not used here)
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = 'Xchaosweaver24!'
-    MYSQL_DB = 'itflask'
